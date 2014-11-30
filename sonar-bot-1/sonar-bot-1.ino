bc92f99895b3a0204adfe7ef6c9c75d9b4657668
johnrpowell <powell.johnr@gmail.com>
2014-11-30 17:10:05 -0600

    Changed to single sonar sweep and added status led

--- sonar-bot-1/sonar-bot-1.ino
+++ sonar-bot-1/sonar-bot-1.ino
@@ -9,14 +9,21 @@ Servo servo;
 Maxbotix rangeSensorAD(A0, Maxbotix::AN, Maxbotix::LV, Maxbotix::BEST, 9);
 int pos = 0;
 int i = 0;
-float scanReadingsA[17];
-float scanReadingsB[17];
+float scanReadings[17];
 
 void setup()
 {
   Serial.begin(9600);
   servo.attach(10);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  
   rangeSensorAD.setADSampleDelay(10);
+  servo.write(0);
+  setColor(255,0,0);
+  delay(3000);
+  setColor(0,255,0);
 }
 
 void loop()
@@ -24,27 +31,23 @@ void loop()
   for(pos = 0; pos < 160; pos += 10) 
   {                               
     servo.write(pos);
-    scanReadingsA[i] = rangeSensorAD.getRange();
-    i++;
-    delay(100);
-  }
-  i = 0;
-  for(pos = 160; pos > 0; pos -= 10)
-  {                                
-    servo.write(pos);
-    scanReadingsB[i] = rangeSensorAD.getRange();
+    scanReadings[i] = rangeSensorAD.getRange();
     i++;
     delay(100);
   }
   i = 0;
   servo.write(0);
   pos = 0;
-  Serial.println("A: ");
-  printArray(scanReadingsA, 15);
-  Serial.println("B: ");
-  printArray(scanReadingsB, 15);
-  Serial.println("=====");
-  delay(2000);
+  printArray(scanReadings, 15);
+  Serial.println(" ");
+  delay(3000);
+}
+
+void setColor(int red, int green, int blue)
+{
+  analogWrite(A1, red);
+  analogWrite(A2, green);
+  analogWrite(A3, blue);  
 }
 
 void printArray(float* array, uint8_t array_size) {
