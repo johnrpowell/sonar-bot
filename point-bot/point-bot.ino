a3372ebcc09edc1bda5bb1c081e7c12bdd80f9e0
johnrpowell <powell.johnr@gmail.com>
2014-11-30 20:11:34 -0600

    Bot orientation test.

--- point-bot/point-bot.ino
+++ point-bot/point-bot.ino
@@ -1,28 +1,23 @@
 #include <RedBot.h>
 #include <RedBotSoftwareSerial.h>
 
-#define turnSpd 100
-
 RedBotMotor motor;
-int pivotDelays[] = {350, 300, 250, 200, 150, 100, 50, 25, 25, 50, 100, 150, 200, 250, 300, 350};
+int pivotDelays[] = {425, 372, 319, 266, 213, 160, 107, 54, 0, 54, 107, 160, 213, 266, 319, 372, 425};
+int i = 9;
+int turnSpd = -100;
 
 void setup()
 {
   Serial.begin(9600);
+  delay(2000);
 }
 
 void loop()
 {
-  turnBot(8);
-  delay(3000);
-  turnBot(9);
-  delay(3000);
-  turnBot(10);
-  delay(3000);
-  turnBot(11);
-  delay(3000);
-  turnBot(12);
-  delay(3000);
+  motor.pivot(turnSpd);
+  delay(pivotDelays[4]);
+  motor.brake();
+  delay(10000);
 }
 
 void turnBot(int d)
